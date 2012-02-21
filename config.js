294486bfe0f84c5082b3f2f3d88dbfe26d75c277
Sebastian Kippe <info@sebastiankippe.de>
2012-02-21 22:31:13 +0100

    Fix config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ exports.config = {
   redisHost: (process.env.ES_REDIS_HOST === undefined ? 'localhost' : process.env.ES_REDIS_HOST),
   redisPort: (process.env.ES_REDIS_PORT === undefined ? 6379 : process.env.ES_REDIS_PORT),
   redisPwd:  (process.env.ES_REDIS_PWD  === undefined ? '' : process.env.ES_REDIS_PWD),
-  origin:    (process.env.ES_HOST       === undefined ? 'localhost' : process.env.ES_HOST),
+  host:      (process.env.ES_HOST       === undefined ? 'localhost' : process.env.ES_HOST),
   port:      (process.env.ES_PORT       === undefined ? 4000 : process.env.ES_PORT)
 };
